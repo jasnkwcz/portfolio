f9e8bbbd4323dd1c231a1e34cf584fed9f3d56d6
jasnkwcz <jasnkwcz@gmail.com>
2021-08-06 08:05:07 -0700

    added an 'about me' page and routing

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const About = () => {
+    return (
+        <Layout pageTitle='About me'>
+            <p>
+            I am a beginning software engineer with interests focused on cloud services as well as systems programming. I am currently finishing my second degree in computer science from Oregon State University. I live in Oregon with my fiancee, Maria, and our two dogs, Laika and Olive. We enjoy spending our free time traveling and cooking. I have a large collection of board games, and I have an interest in making games of my own as well.
+            </p>
+        </Layout>
+    )
+}
+
+export default About
